25d25f30f658140daf8b9dfa1f696d9d4195b1e6
nevada_scout <joe@jckd.co.uk>
2018-12-14 10:32:07 +0100

    Added skeleton server code for symbol, hover and definition providers

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,8 +4,21 @@
  * ------------------------------------------------------------------------------------------ */
 
 import {
-    createConnection, TextDocuments, ProposedFeatures, TextDocumentSyncKind
+    createConnection,
+    TextDocuments,
+    ProposedFeatures,
+    SymbolInformation,
+    Hover,
+    Definition,
+    SymbolKind
 } from "vscode-languageserver";
+import { HandlerResult } from "vscode-jsonrpc";
+
+// import * as acorn from "acorn";
+
+// const js = "";
+// const options: acorn.Options = {};
+// acorn.parse(js, options);
 
 // Creates the LSP connection
 let connection = createConnection(ProposedFeatures.all);
@@ -24,13 +37,76 @@ documents.listen(connection);
 connection.onInitialize((params) => {
     workspaceFolder = params.rootUri;
     connection.console.log(`[Server(${process.pid}) ${workspaceFolder}] Started and initialize received`);
+
+    // TODO: start parsing & building tree for workspaceFolder
+    //  - all components + link to HTML file
+    //  - services
+    //  - directives
+
     return {
         capabilities: {
-            textDocumentSync: {
-                openClose: true,
-                change: TextDocumentSyncKind.None
-            }
+            // completionProvider: {
+            //     resolveProvider: false,
+            //     triggerCharacters: ["{", "."],
+            // },
+
+            definitionProvider: true,
+            documentSymbolProvider: true,
+            workspaceSymbolProvider: true,
+            hoverProvider: true,
+
+            // signatureHelpProvider: {
+            //     triggerCharacters: ["(", ","]
+            // },
+
+            // textDocumentSync: {
+            //     openClose: true,
+            //     change: TextDocumentSyncKind.None
+            // }
         }
     }
 });
 connection.listen();
+
+// TODO: replace inline handler function with reference to class method
+connection.onDocumentSymbol((params, cancelToken): Promise<SymbolInformation[]> => {
+    return new Promise((resolve, reject) => {
+        const symbols = [{
+            name: "TEST",
+            containerName: "",
+            kind: SymbolKind.Class,
+            location: {
+                uri: "test",
+                range: {
+                    start: {
+                        character: 0,
+                        line: 0
+                    },
+                    end: {
+                        character: 0,
+                        line: 0
+                    }
+                }
+            }
+        }];
+
+        resolve(symbols);
+    });
+});
+
+// TODO: replace inline handler function with reference to class method
+connection.onWorkspaceSymbol((params, cancelToken): SymbolInformation[] => {
+    return null;
+});
+
+// TODO: replace inline handler function with reference to class method
+connection.onHover((params, cancelToken): HandlerResult<Hover, void> => {
+    // TODO: get js-doc comment info
+    // TODO: make this work within HTML files
+    return null;
+});
+
+// TODO: replace inline handler function with reference to class method
+connection.onDefinition((params, cancelToken): HandlerResult<Definition, void> => {
+    return null;
+});
